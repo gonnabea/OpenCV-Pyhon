65122fb7f1f8a9f74fd4d99f3ebce780b227f704
Choi Jiwon <winning1993@naver.com>
2020-10-05 11:01:32 +0900

    Color Detection (project1)

--- chapter1.py
+++ chapter1.py
@@ -1,18 +1,18 @@
 import cv2
 
-img = cv2.imread("Resources/gentlemen.jpg")
-
-cv2.imshow("Output", img)
-
-cv2.waitKey(0) # 이미지 불러오기
-
-cap = cv2.VideoCapture("Resources/sample.mp4")
-
-while True:
-    success, img = cap.read()
-    cv2.imshow("Video", img)
-    if cv2.waitKey(delay=1) & 0xFF == ord('q'):
-        break # 특정 경로로 비디오 불러오기
+# img = cv2.imread("Resources/gentlemen.jpg")
+#
+# cv2.imshow("Output", img)
+#
+# cv2.waitKey(0) # 이미지 불러오기
+#
+# cap = cv2.VideoCapture("Resources/sample.mp4")
+#
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow("Video", img)
+#     if cv2.waitKey(delay=1) & 0xFF == ord('q'):
+#         break # 특정 경로로 비디오 불러오기
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -24,3 +24,5 @@ while True:
     cv2.imshow("Video", img)
     if cv2.waitKey(delay=1) & 0xFF == ord('q'):
         break # 웹캠 사용
+
+
---
+++ project1.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10,150)
+
+myColors = [[5,107,0,19,255,255],#[orange]
+            [133,56,0,159,156,255],#[purple]
+            [57,76,0,100,255,255],#[green]
+[0,127,117,179,255,255]#[blue]
+            ]
+# [0,127,117,179,255,255]#[blue]
+def findColor(img,myColors):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    for color in myColors:
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv2.inRange(imgHSV, lower, upper)
+        cv2.imshow(str(color[0]),mask)
+
+while True:
+    success, img = cap.read()
+    findColor(img, myColors)
+    cv2.imshow("Result",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
